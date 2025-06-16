2595c7e6350c46c1f759f31613f7b5cb5ba35602
Juan Villarejo <jbtwist92@gmail.com>
2025-06-16 19:01:26 +0200

    Make System Pallet Generic

--- src/system.rs
+++ src/system.rs
@@ -1,23 +1,33 @@
-use std::collections::BTreeMap;
-
-type AccountId = String;
-type BlockNumber = u32;
-type Nonce = u32;
+use num::{Integer, Zero};
+use std::{collections::BTreeMap, ops::AddAssign};
 
 /// This is the System Pallet.
 /// It handles low level state needed for your blockchain.
 #[derive(Debug)]
-pub struct Pallet {
+pub struct Pallet<BlockNumber, AccountId, Nonce> {
 	/// The current block number.
 	block_number: BlockNumber,
 	/// A map from an account to their nonce.
 	nonce: BTreeMap<AccountId, Nonce>,
 }
 
-impl Pallet {
+/*
+	TODO:
+	The generic types need to satisfy certain traits in order to be used in the functions below.
+	See if you can figure them out yourself.
+
+	NOTE: You might need to adjust some of the functions below to satisfy the borrow checker.
+*/
+
+impl<BlockNumber, AccountId, Nonce> Pallet<BlockNumber, AccountId, Nonce>
+where
+	BlockNumber: Integer + Zero + AddAssign + Copy + Clone,
+	AccountId: Ord + Clone,
+	Nonce: Integer + Zero + Ord + Copy + Clone,
+{
 	/// Create a new instance of the System Pallet.
 	pub fn new() -> Self {
-		Self { block_number: 0, nonce: BTreeMap::new() }
+		Self { block_number: BlockNumber::zero(), nonce: BTreeMap::new() }
 	}
 
 	/// Get the current block number.
@@ -28,14 +38,14 @@ impl Pallet {
 	// This function can be used to increment the block number.
 	// Increases the block number by one.
 	pub fn inc_block_number(&mut self) {
-		self.block_number += 1;
+		self.block_number += BlockNumber::one();
 	}
 
 	// Increment the nonce of an account. This helps us keep track of how many transactions each
 	// account has made.
 	pub fn inc_nonce(&mut self, who: &AccountId) {
-		let nonce: Nonce = *self.nonce.get(who).unwrap_or(&0);
-		let new_nonce = nonce + 1;
+		let nonce: Nonce = *self.nonce.get(who).unwrap_or(&Nonce::zero());
+		let new_nonce = nonce + Nonce::one();
 		self.nonce.insert(who.clone(), new_nonce);
 	}
 }
@@ -44,7 +54,11 @@ impl Pallet {
 mod test {
 	#[test]
 	fn init_system() {
-		let mut system = super::Pallet::new();
+		/*
+			TODO:
+			When creating an instance of `Pallet`, you should explicitly define the types you use.
+		*/
+		let mut system = super::Pallet::<u32, String, u32>::new();
 		system.inc_block_number();
 		system.inc_nonce(&"alice".to_string());
 
